45ea24fad71ca9cccbaeed6c542c675eaeed5826
Mike Kornelson <mike@koretech.ca>
2014-12-01 10:31:38 -0600

    Upgraded to Meteor 1.0 Upgraded to Semantic-UI 1.0.1 Still working with server side validation

--- package.js
+++ package.js
@@ -3,27 +3,27 @@ var client = 'client', server = 'server', both = ['client', 'server'];
 Package.describe({
 	name: 'krt:inplace',
 	summary: 'Koretech Inplace Editor Package',
-	version: '0.1.0',
+	version: '0.1.1',
 	git: 'https://github.com/koretech/meteor-krt-inplace.git'
 });
 
 Package.onUse(function(api){
 
-	api.versionsFrom('METEOR@0.9.4');
+	api.versionsFrom('METEOR@1.0');
 
 	api.use([
 		'krt:core@0.1.0',
 		'templating',
 		'underscore',
 		'less',
-		'nooitaf:semantic-ui-less@0.19.3',
+		'nooitaf:semantic-ui@1.0.1',
 		'mquandalle:bower@0.1.11',
-		'aldeed:simple-schema@0.7.0'
+		'aldeed:simple-schema@1.1.0'
 	], both);
 
 	api.imply([
-		'krt:core@0.1.0',
-		'nooitaf:semantic-ui-less'
+		'krt:core',
+		'nooitaf:semantic-ui'
 	]);
 
 	api.addFiles([
